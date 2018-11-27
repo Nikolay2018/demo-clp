6dfa1ff9fe7a136f45eeb0af5b121014bac23333
Developer <developer@dev.loc>
2018-11-27 04:40:47 +0200

    Add dashboard layout.

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { HeaderComponent } from './containers/header/header.component';
+import { MatButtonModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
-  declarations: [DashboardPageComponent],
+  declarations: [DashboardPageComponent, HeaderComponent],
   imports: [
     CommonModule,
+    MatMenuModule,
+    MatButtonModule,
     DashboardRoutingModule
   ]
 })
-export class DashboardModule { }
+export class DashboardModule {
+}
